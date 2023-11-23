83f25e8f9c0ee0f653beef602cf1112d51265a30
robisoncrisbrito <robison.brito@gmail.com>
2023-11-22 22:06:33 -0300

    lista pronto

--- app/src/main/java/br/edu/utfpr/affirmation/adapter/ItemAdapter.kt
+++ app/src/main/java/br/edu/utfpr/affirmation/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.edu.utfpr.affirmation.R
@@ -14,6 +15,7 @@ class ItemAdapter (private val context : Context, private val dataset : List<Aff
 
         class ItemViewHolder (private val view : View ) : RecyclerView.ViewHolder ( view ) {
             val textView : TextView = view.findViewById( R.id.item_title )
+            val imageView : ImageView = view.findViewById( R.id.item_image )
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -29,6 +31,7 @@ class ItemAdapter (private val context : Context, private val dataset : List<Aff
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[ position ]
         holder.textView.text = context.resources.getString( item.stringResourdId )
+        holder.imageView.setImageResource( item.imageResourceId )
     }
 
 }
