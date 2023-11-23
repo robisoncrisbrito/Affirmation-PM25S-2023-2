84d6518290bb0f677bff4d5d20566d127c85ac96
robisoncrisbrito <robison.brito@gmail.com>
2023-11-22 22:03:06 -0300

    lista pronto

--- app/src/main/java/br/edu/utfpr/affirmation/model/Affirmation.kt
+++ app/src/main/java/br/edu/utfpr/affirmation/model/Affirmation.kt
@@ -1,3 +1,9 @@
 package br.edu.utfpr.affirmation.model
 
-data class Affirmation (val stringResourdId : Int )
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation (
+    @StringRes  val stringResourdId : Int,
+    @DrawableRes val imageResourceId: Int
+)
