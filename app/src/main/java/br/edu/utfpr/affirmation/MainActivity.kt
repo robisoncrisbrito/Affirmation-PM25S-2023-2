395fce7e4952eca8d260070177a004c9983f8a5f
robisoncrisbrito <robison.brito@gmail.com>
2023-11-22 21:45:13 -0300

    lista pronto

--- app/src/main/java/br/edu/utfpr/affirmation/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/affirmation/MainActivity.kt
@@ -2,10 +2,16 @@ package br.edu.utfpr.affirmation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import br.edu.utfpr.affirmation.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvContador = findViewById<TextView>( R.id.tvContador )
+        tvContador.text = Datasource().loadAffirmation().size.toString()
+
     }
 }
